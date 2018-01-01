b15007eb68c57dc942cd9358ef101404394dd922
Alex Alex Miranda <34800889+mirantrix@users.noreply.github.com>
2017-12-31 23:54:49 -0800

    Delete checkPalindrome.js

--- Code Fights JavaScript/checkPalindrome.js
+++
@@ -1,8 +0,0 @@
-function checkPalindrome(inputString) {
-    var reverse = inputString.split("").reverse().join("");
-    if(inputString === reverse){
-       return true;
-       } else {
-       return false;
-       }
-}
